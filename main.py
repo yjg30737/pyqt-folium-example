188c9eead3b1841dd6ba432992c8f1da61253673
Jung Gyu Yoon <yjg30737@gmail.com>
2023-02-01 10:23:21 +0900

    Make it able to switch between the tiles

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import folium
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QRadioButton, QWidget, QHBoxLayout, QVBoxLayout
 
 
 class MainWindow(QMainWindow):
@@ -14,16 +14,58 @@ class MainWindow(QMainWindow):
     def __initUi(self):
         self.setWindowTitle('PyQt & Folium')
 
-        m = folium.Map()
-        m.save("map.html")
+        self.__m = folium.Map(tiles='openstreetmap')
+        self.__m.save("map.html")
 
         cur_dir = os.path.dirname(__file__)
         map_filename = os.path.join(cur_dir, 'map.html').replace(os.path.sep, posixpath.sep)
 
-        view = QWebEngineView()
-        view.load(QUrl.fromLocalFile(map_filename))
+        self.__view = QWebEngineView()
+        self.__view.load(QUrl.fromLocalFile(map_filename))
 
-        self.setCentralWidget(view)
+        # Create the radio buttons for each tile provider
+        stamen_toner = QRadioButton('Stamen Toner')
+        stamen_toner.clicked.connect(lambda: self.switch_tiles(stamen_toner.text()))
+        openstreetmap = QRadioButton('OpenStreetMap')
+        openstreetmap.clicked.connect(lambda: self.switch_tiles(openstreetmap.text()))
+        openstreetmap.setChecked(True)
+        stamen_terrain = QRadioButton('Stamen Terrain')
+        stamen_terrain.clicked.connect(lambda: self.switch_tiles(stamen_terrain.text()))
+
+        lay = QHBoxLayout()
+
+        # Add the radio buttons to the layout
+        lay.addWidget(stamen_toner)
+        lay.addWidget(openstreetmap)
+        lay.addWidget(stamen_terrain)
+        lay.setContentsMargins(0, 0, 0, 0)
+
+        navWidget = QWidget()
+        navWidget.setLayout(lay)
+
+        lay = QVBoxLayout()
+        lay.addWidget(navWidget)
+        lay.addWidget(self.__view)
+
+        mainWidget = QWidget()
+        mainWidget.setLayout(lay)
+
+        self.setCentralWidget(mainWidget)
+
+    # Create a function to switch the folium tiles when the radio buttons are clicked
+    def switch_tiles(self, text):
+        if text == 'Stamen Toner':
+            folium.TileLayer('Stamen Toner').add_to(self.__m)
+            self.__m.save('map.html')
+            self.__view.reload()
+        elif text == 'OpenStreetMap':
+            folium.TileLayer('OpenStreetMap').add_to(self.__m)
+            self.__m.save('map.html')
+            self.__view.reload()
+        elif text == 'Stamen Terrain':
+            folium.TileLayer('Stamen Terrain').add_to(self.__m)
+            self.__m.save('map.html')
+            self.__view.reload()
 
 
 if __name__ == "__main__":
